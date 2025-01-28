3c830e545033196a2dd2b9e71f5aecf5081e6bd0
User8755 <110420113+User8755@users.noreply.github.com>
2025-01-28 20:53:13 +0500

    fix mongo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(fileUpload());
 
-//mongoose.connect(MONGODB);
+mongoose.connect(MONGODB);
 
 app.use(
   cors({
